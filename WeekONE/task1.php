82fb6154b71c2a180715e7acc73865ea0627a2d2
Egorov Alexey aka Frontmaster <asserxlv@gmail.com>
2022-06-14 22:45:13 +0900

    ДЗ для проверки
     Initial commit

---
+++ WeekONE/task1.php
@@ -0,0 +1,10 @@
+<?php
+
+// task 1
+$name = 'Алексей';
+$age = 26;
+
+echo "Меня зовут: $name <br> Мне $age лет";
+echo "<br>";
+echo '"!|/\'"\\';
+
---
+++ WeekONE/task2.php
@@ -0,0 +1,8 @@
+<?php
+// task 2
+const pic = 80;
+const mark = 23;
+const pen = 40;
+
+$color = pic - mark - pen;
+echo "$color рисунков были сделаны краской";
